992cc7b59c08989afcdbc99fc498c163ad736204
an7-seven <vietann.0409@gmail.com>
2025-06-15 14:57:36 +0700

    update-login
    15/06

---
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/DTO/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.swp391.superapp.bloodsupport.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/DTO/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.swp391.superapp.bloodsupport.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String message;
+    private String username;
+    private String role;
+}
