8fe83bdeb4db057088bbe8f1d0c7e150dfa7308c
Hồ Trần Quân <116471791+ShiroSs00@users.noreply.github.com>
2025-06-16 14:08:36 +0700

    Delete back-end directory

--- back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/repository/HealthCheckRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swp391.superapp.bloodsupport.repository;
-
-import com.swp391.superapp.bloodsupport.entity.HealthCheck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthCheckRepository extends JpaRepository<HealthCheck, Long> {
-}
