4b471085dd10be13371116a5379637eacbbc7cd2
an7-seven <vietann.0409@gmail.com>
2025-06-16 14:09:27 +0700

    delete

--- back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/repository/AfterDonationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swp391.superapp.bloodsupport.repository;
-
-import com.swp391.superapp.bloodsupport.entity.AfterDonationBlood;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AfterDonationRepository extends JpaRepository<AfterDonationBlood, Long> {
-}
