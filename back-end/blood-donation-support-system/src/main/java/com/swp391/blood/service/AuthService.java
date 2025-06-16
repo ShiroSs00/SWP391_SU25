c9bddfbf859b61600f11a548930908e8c364ff34
an7-seven <vietann.0409@gmail.com>
2025-06-16 15:14:46 +0700

    u

---
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/blood/service/AuthService.java
@@ -0,0 +1,85 @@
+package com.swp391.blood.service;
+
+import com.swp391.superapp.bloodsupport.DTO.log.LoginRequest;
+import com.swp391.superapp.bloodsupport.DTO.log.LoginResponse;
+import com.swp391.superapp.bloodsupport.entity.Account;
+import com.swp391.superapp.bloodsupport.repository.AccountRepository;
+import com.swp391.superapp.bloodsupport.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        try{
+            //tìm account theo username hoặc email
+            if(loginRequest == null || loginRequest.getUsername() == null || loginRequest.getPassword() == null){
+                return new LoginResponse(null, "Username or password is missing", null, null);
+
+            }
+
+            Optional<Account> accountOtp = accountRepository.findByUserName(loginRequest.getUsername());
+
+
+            if(accountOtp.isEmpty()){
+                accountOtp = accountRepository.findByEmail(loginRequest.getUsername());
+            }
+
+            if(accountOtp.isEmpty()){
+                return new LoginResponse(null,"User not found", null, null);
+            }
+
+            Account account = accountOtp.get();
+
+            //kiểm tra account có active không?
+            if(!account.isActive()){
+                return new LoginResponse(null,"Account is not active", null, null);
+            }
+
+            // kiểm tra password
+//            if(!passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())){
+//                return new LoginResponse(null,"Wrong password", null, null);
+//            }
+            if(!loginRequest.getPassword().equals(account.getPassword())){
+                return new LoginResponse(null,"Password is incorrect", null, null);
+            }
+
+            //tạo jwt token
+            String token = jwtUtil.generateToken(account.getUserName());
+
+            return new LoginResponse(token,"Login successful", account.getUserName(),account.getRole().getRole());
+        }catch (Exception e) {
+            return new LoginResponse(null, "Login failed: " + e.getMessage(), null, null);
+        }
+    }
+
+    public String logout(String token) {
+        return "logout successful";
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            String username = jwtUtil.extractUsername(token);
+            return jwtUtil.validateToken(token, username);
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+
+}
