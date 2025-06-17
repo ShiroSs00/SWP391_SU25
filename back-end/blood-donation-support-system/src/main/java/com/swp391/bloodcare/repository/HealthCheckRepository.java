4e89af04925e2353199934b3dbe2633dff06caa4
ThanhDucP <duc49531@gmail.com>
2025-06-17 12:06:51 +0700

    update

--- back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/repository/HealthCheckRepository.java
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/repository/HealthCheckRepository.java
@@ -3,5 +3,8 @@ package com.swp391.bloodcare.repository;
 import com.swp391.bloodcare.entity.HealthCheck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HealthCheckRepository extends JpaRepository<HealthCheck, Long> {
+    Optional<HealthCheck> findByDonationRegistration_RegistrationId(int registrationId);
 }
