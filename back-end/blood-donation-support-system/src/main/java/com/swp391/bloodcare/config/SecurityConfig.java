14decba9a8e7276b78d5768233131a39d8d6806e
ThanhDucP <duc49531@gmail.com>
2025-06-17 13:22:31 +0700

    Update SecurityConfig.java

--- back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/config/SecurityConfig.java
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/swagger-resources/**",
-                                "/webjars/**"
+                                "/webjars/**",
+                                "https://swp391-su25-1.onrender.com/**"
                         ).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
