c863a1f5edf94c17d9594f423e1f3a8c40f6c225
ThanhDucP <duc49531@gmail.com>
2025-07-14 18:21:58 +0700

    update schedule

--- back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/service/NotificationService.java
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/bloodcare/service/NotificationService.java
@@ -113,7 +113,6 @@ public class NotificationService {
                 .collect(Collectors.toList());
     }
 
-    // ======== Support methods ========
 
     private Account getAccountOrThrow(String accountId) {
         return accountRepository.findById(accountId)
